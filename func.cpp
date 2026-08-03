94530bde1056b1ce9458832a264e30142fb03090
siddhichougule4777-web <siddhichougule4777@gmail.com>
2026-08-03 03:51:22 +0000

    func program.cpp

---
+++ func.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int area(int);
+int area(int, int);
+float area(float);
+
+int main()
+{
+    cout << "area for side=5 " << area(5);
+    cout << "area for length=5,breadth=10 " << area(5, 10);
+    cout << "area for circle " << area(3.5f);
+
+    return 0;
+}
+
+int area(int side)
+{
+    return side * side;
+}
+
+int area(int h, int b)
+{
+    return h * b;
+}
+
+float area(float radius)
+{
+    return (3.14 * radius * radius);
+}
